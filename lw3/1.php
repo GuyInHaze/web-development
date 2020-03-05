f59209bed34445a196bd7bc4b0d613bbab02231a
guyinhaze <guyinhaze@gmail.com>
2020-03-05 09:31:45 +0300

    first task modified

--- lw3/1.php
+++ lw3/1.php
@@ -1,41 +1,55 @@
 <?php
-  header("Content-Type: text/plain");
 
-  function getGETParameter($parameter) {
-     return isset($_GET[$parameter]) ? (string)$_GET[$parameter] : null;
-  };
+header("Content-Type: text/plain");
+$text = getGETParameter('text');    
+if($text === null)
+{
+    echo 'incorrect data'; 
+}
+else
+{
+    if(strlen($text) != 0)
+    { 
+        echo getGETParameter('text'), ' - raw string';
+        echo "\n";
+        echo removeExtraBlanks($text), ' - converted string';
+    }
+    else
+    {
+        echo 'no data';
+    }
+} 
+ 
+function getGETParameter(string $parameter): ?string 
+{
+    return isset($_GET[$parameter]) ? (string)$_GET[$parameter] : null;
+}
 
-  function removeExtraBlanks($parametr){
-  	$i = 0;
-  	while ($i < strlen($parametr) & $parametr[$i] == ' '){
-          $i++;
-        };
-  	while ($i < strlen($parametr)) {
-    	 if ($parametr[$i] != ' ') {
-      	   echo $parametr[$i];
-      	   $i++;
-    	 } 
-   	 else {
-           while ($parametr[$i] == ' ' & $i < strlen($parametr)){
-             $i++;
-           };
-           if ($i != strlen($parametr)){
-             echo ' ';
-           };
-         };
-        };
-       echo '?';//маркер конца строки, чтобы обозначить обрезанные в конце пробелы 
-     }; 
-
-  $text = getGETParameter('text');    
-  if($text === null){
-  echo 'incorrect data'; 
-}else{
-  if(strlen($text) != 0){ 
-  echo getGETParameter('text'), ' - raw string';
-  echo "\n";
-  echo removeExtraBlanks($text), ' - converted string';
-}else{
-  echo 'no data';
-};
-};  
+function removeExtraBlanks(string $parametr): ?string
+{
+    $i = 0;
+    while ($i < strlen($parametr) & $parametr[$i] == ' ')
+    {
+        $i++;
+    }
+    while ($i < strlen($parametr)) 
+    {
+        if ($parametr[$i] != ' ')
+        {
+      	    echo $parametr[$i];
+      	    $i++;
+    	} 
+   	else 
+        {
+            while ($parametr[$i] == ' ' & $i < strlen($parametr))
+            {
+                $i++;
+            }
+            if ($i != strlen($parametr))
+            {
+                echo ' ';
+            }
+        }
+     }
+    echo '?';//чтобы обозначить обрезанные в конце пробелы 
+} 
