983f4676ca16d13bb6b9cc368a50f9b1ee67b720
student <student@school.isphera.ru>
2020-03-05 08:58:41 +0300

    first modified task

--- lw3/1.php
+++ lw3/1.php
@@ -1,25 +1,41 @@
 <?php
   header("Content-Type: text/plain");
+
   function getGETParameter($parameter) {
-     return isset($_GET[$parameter]) ? $_GET[$parameter] : null;
+     return isset($_GET[$parameter]) ? (string)$_GET[$parameter] : null;
   };
-  $text = getGETParameter('text');
-      function removeExtraBlanks($parametr){
+
+  function removeExtraBlanks($parametr){
   	$i = 0;
-  	while ($i < strlen($parametr) & $parametr[$i] == ' ') $i++;
+  	while ($i < strlen($parametr) & $parametr[$i] == ' '){
+          $i++;
+        };
   	while ($i < strlen($parametr)) {
     	 if ($parametr[$i] != ' ') {
       	   echo $parametr[$i];
       	   $i++;
     	 } 
    	 else {
-           while ($parametr[$i] == ' ' & $i < strlen($parametr)) $i++;
-           if ($i != strlen($parametr)) echo ' ';
+           while ($parametr[$i] == ' ' & $i < strlen($parametr)){
+             $i++;
+           };
+           if ($i != strlen($parametr)){
+             echo ' ';
+           };
          };
         };
-       echo '?'; 
+       echo '?';//маркер конца строки, чтобы обозначить обрезанные в конце пробелы 
      }; 
-  echo getGETParameter('text');
+
+  $text = getGETParameter('text');    
+  if($text === null){
+  echo 'incorrect data'; 
+}else{
+  if(strlen($text) != 0){ 
+  echo getGETParameter('text'), ' - raw string';
   echo "\n";
-  echo removeExtraBlanks($text);
-  
+  echo removeExtraBlanks($text), ' - converted string';
+}else{
+  echo 'no data';
+};
+};  
