6f7cd3f85a33e93669502342a97c53bf8e37d46c
guyinhaze <guyinhaze@gmail.com>
2020-04-09 08:26:01 +0300

    proga

---
+++ lw7/src/js/script1.js
@@ -0,0 +1,56 @@
+let n;
+let k;
+let isPrime;  
+function isPrimeNumber(n) {
+  if (typeof n == 'number'){
+
+      for (let i = 2; i <= n; i++) {
+
+        isPrime = true;
+        for (let j = 2; j < i; j++) {
+
+          if (i % j == 0) {
+            isPrime = false;
+            break;
+          }
+
+        } 
+
+      }
+      
+      if (isPrime) {
+        console.log(n + ' is prime number');
+      } else {
+        console.log(n + ' is not prime number');
+      }
+
+  } else if (typeof n == 'object') {
+
+    for(k of n) {
+
+      for (let i = 2; i <= k; i++) {
+
+        isPrime = true;
+        for (let j = 2; j < i; j++) {
+
+          if (i % j == 0) {
+            isPrime = false;
+            break;
+          }
+
+        }
+
+      }  
+
+      if (isPrime) {
+        console.log(k + ' is prime number')
+      } else {
+        console.log(k + ' is not prime number')
+      }
+
+    }
+
+  } else {
+    console.log('incorrect input data');
+  }
+}
