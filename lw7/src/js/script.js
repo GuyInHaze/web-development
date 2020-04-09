3ee900a9624b3788642d301c4b499afd75597bbe
guyinhaze <guyinhaze@gmail.com>
2020-04-09 09:31:16 +0300

    rewrite

---
+++ lw7/src/js/script.js
@@ -0,0 +1,56 @@
+ function isPrimeNumber(let n) {
+   let n;
+   let isPrime;
+
+   if (typeof n == 'number') {
+
+     for (let i = 2; i <= n; i++) {
+
+       isPrime = true;
+       for (let j = 2; j < i; j++) {
+
+         if (i % j == 0) {
+           isPrime = false;
+           break;
+         }
+
+       }
+
+     }
+
+   if (isPrime) {
+     console.log(n + ' is prime number');
+   } else {
+     console.log(n + ' is not prime number');
+   }
+
+   } else if (typeof n == 'object') {
+
+     for (let k of n) {
+
+       for (let i = 2; i <= k; i++) {
+
+         isPrime = true;
+         for (let j = 2; j < i; j++) {
+
+           if (i % j == 0) {
+             isPrime = false;
+             break;
+           }
+
+         }
+
+       }
+
+       if (isPrime) {
+         console.log(k + ' is prime number')
+       } else {
+         console.log(k + ' is not prime number')
+       }
+ 
+     }
+
+   } else {
+     console.log('incorrect input data');
+   }
+ }
