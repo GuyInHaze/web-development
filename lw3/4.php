ce4235be0d40a36c6590cf1cf8ea3cc505274dcc
GuyInHaze <50462277+GuyInHaze@users.noreply.github.com>
2020-05-28 20:19:33 +0300

    Update 4.php

--- lw3/4.php
+++ lw3/4.php
@@ -1,29 +1,29 @@
 <?php
-  header("Content-Type: text/plain");
+    header("Content-Type: text/plain");
 
-  $email = getGETParameter('email');
-  $first_name = getGETParameter('first_name');
-  $last_name = getGETParameter('last_name');
-  $age = getGETParameter('age');
+    $email = getGETParameter('email');
+    $first_name = getGETParameter('first_name');
+    $last_name = getGETParameter('last_name');
+    $age = getGETParameter('age');
 
-  if ($email == null)
-  {
-  	echo 'Incorrect email data';
-  }
-  else
-  {
-  	 $filename = 'data/'.$email.'.txt';
-  	 if (!file_exists('data/'))
-     {
-       mkdir('data/', 0777, true);
-     }
-  	 $person_data  = 'First name = '.$first_name."\r\n".'Last name = '.$last_name."\r\n".'Age = '.$age;
-  	 $person_data_file = fopen($filename,'w');
-  	 fwrite($person_data_file, $person_data);
-  	 fclose($person_data_file);
+    if ($email == null)
+    {
+  	    echo 'Incorrect email data';
+    }
+    else
+    {
+  	    $filename = 'data/' . $email . '.txt';
+  	if (!file_exists('data/'))
+    {
+        mkdir('data/', 0777, true);
+    }
+  	$person_data  = 'First name = ' . $first_name . "\r\n" . 'Last name = ' . $last_name . "\r\n" . 'Age = ' . $age;
+  	$person_data_file = fopen($filename, 'w');
+  	fwrite($person_data_file, $person_data);
+  	fclose($person_data_file);
+    }
 
-  }
-  function getGETParameter(string $name): ?string
-  {
-    return isset($_GET[$name]) ? $_GET[$name] : null;
-  }
+    function getGETParameter(string $name): ?string
+    {
+        return isset($_GET[$name]) ? $_GET[$name] : null;
+    }
