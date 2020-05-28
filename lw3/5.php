bfacc2f85a5aa2abb48f923b785fe6a303bd9c62
GuyInHaze <50462277+GuyInHaze@users.noreply.github.com>
2020-05-28 20:20:15 +0300

    Update 5.php

--- lw3/5.php
+++ lw3/5.php
@@ -1,28 +1,29 @@
 <?php
 
-  $email = getGETParameter('email');
+    $email = getGETParameter('email');
   
-  if ($email == null)
-  {
-  	echo 'incorrect email data';
-  } 
-  else
-  {
-    $filename = 'data/'.$email.'.txt';
-    if (file_exists($filename))
+    if ($email == null)
     {
-      $person_data_file = fopen($filename, 'r');
-      while (!feof($person_data_file))
-      {
-      	echo fgets($person_data_file)."<br/>";
-      }
+  	    echo 'incorrect email data';
     } 
     else
     {
-      echo 'such person does not exist';
+        $filename = 'data/' . $email . '.txt';
+        if (file_exists($filename))
+        {
+            $person_data_file = fopen($filename, 'r');
+            while (!feof($person_data_file))
+            {
+      	        echo fgets($person_data_file) . "<br/>";
+            }
+        } 
+        else
+        {
+            echo 'such person does not exist';
+        }
     }
-  }
-  function getGETParameter(string $name): ?string
-  {
-    return isset($_GET[$name]) ? $_GET[$name] : null;
-  }
+    
+    function getGETParameter(string $name): ?string
+    {
+        return isset($_GET[$name]) ? $_GET[$name] : null;
+    }
