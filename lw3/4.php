f228a520c6397613ac7524dfdf5b6bdd5a2f2e3d
GuyInHaze <50462277+GuyInHaze@users.noreply.github.com>
2020-05-28 21:04:02 +0300

    Update 4.php

--- lw3/4.php
+++ lw3/4.php
@@ -1,29 +1,29 @@
 <?php
-    header("Content-Type: text/plain");
+header("Content-Type: text/plain");
 
-    $email = getGETParameter('email');
-    $first_name = getGETParameter('first_name');
-    $last_name = getGETParameter('last_name');
-    $age = getGETParameter('age');
+$email = getGETParameter('email');
+$firstName = getGETParameter('first_name');
+$lastName = getGETParameter('last_name');
+$age = getGETParameter('age');
 
-    if ($email == null)
-    {
-  	    echo 'Incorrect email data';
-    }
-    else
-    {
-  	    $filename = 'data/' . $email . '.txt';
-  	if (!file_exists('data/'))
+if ($email == null)
+{
+    echo 'Incorrect email data';
+}
+else
+{
+    $fileName = 'data/' . $email . '.txt';
+    if (!file_exists('data/'))
     {
         mkdir('data/', 0777, true);
     }
-  	$person_data  = 'First name = ' . $first_name . "\r\n" . 'Last name = ' . $last_name . "\r\n" . 'Age = ' . $age;
-  	$person_data_file = fopen($filename, 'w');
-  	fwrite($person_data_file, $person_data);
-  	fclose($person_data_file);
-    }
+    $personData  = 'First name = ' . $firstName . "\r\n" . 'Last name = ' . $lastName . "\r\n" . 'Age = ' . $age;
+    $personDataFile = fopen($fileName, 'w');
+    fwrite($personDataFile, $personData);
+    fclose($personDataFile);
+}
 
-    function getGETParameter(string $name): ?string
-    {
-        return isset($_GET[$name]) ? $_GET[$name] : null;
-    }
+function getGETParameter(string $name): ?string
+{
+    return isset($_GET[$name]) ? $_GET[$name] : null;
+}
