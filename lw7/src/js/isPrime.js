b3791ec96b216f2e0d5721be498d9abd2b64b43a
GuyInHaze <50462277+GuyInHaze@users.noreply.github.com>
2020-05-28 22:42:08 +0300

    Add files via upload

---
+++ lw7/src/js/isPrime.js
@@ -0,0 +1,50 @@
+ function isPrimeNumber(n) {
+   let isPrime;
+
+   if (typeof n == 'number') {
+	 if(n != 0 && n != 1){
+       for (let i = 2; i <= n; i++) {
+         isPrime = true;
+         for (let j = 2; j < i; j++) {
+           if (i % j == 0) {
+             isPrime = false;
+             break;
+           }
+         }
+       }
+	   if (isPrime) {
+         console.log(n + ' is prime number');
+       } else {
+         console.log(n + ' is not prime number');
+       }
+     } else {
+	   console.log(n + ' is neither');
+	 }
+
+   } else if (typeof n == 'object') {
+       for (let k of n) {
+         if((typeof k == 'number') && k != 0 && k != 1) {
+           for (let i = 2; i <= k; i++) {
+             isPrime = true;
+             for (let j = 2; j < i; j++) {
+               if (i % j == 0) {
+                 isPrime = false;
+                 break;
+             }
+           }
+         }
+		   if (isPrime) {
+             console.log(k + ' is prime number');
+           } else {
+             console.log(k + ' is not prime number');
+           }
+		   
+       } else {
+	     console.log(k + ' is neither');
+	   }
+     }
+	 
+   } else {
+     console.log('incorrect input data');
+   }
+ }
